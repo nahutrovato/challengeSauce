b100c8e50ed2738f162e2b2c1f4fd52a9a9fae00
nahuel.trovato <nahuel.trovato@theflock.dev>
2025-01-23 19:33:33 -0300

    Create successful login test

---
+++ cypress/e2e/steps_definitions/Login/successful_login.js
@@ -0,0 +1,24 @@
+import {Given, When, And, Then} from 'cypress-cucumber-preprocessor/steps';
+const Login = require('../../../pages/Login/login_page');
+const Home = require('../../../pages/Home/home_page');
+
+Given('the user logs into the Saucedemo website', () => {
+    cy.visit(Cypress.env('BASE_URL'));
+});
+
+When('the user enters a correct username {string}', (username) => {
+    Login.enterUsername(Cypress.env(username));
+});
+
+And('the user enters the correct password {string}', (password) => {
+    Login.enterPassword(Cypress.env(password));
+});
+
+And('the user clicks the login button', () => {
+    Login.clickLoginButton();
+});
+
+Then('the user correctly verifies the successful login by viewing the hamburger menu {string}', (element) => {
+    Home.isDisplayedElement(element);
+});
+
